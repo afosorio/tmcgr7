94b524be98a18643e89d668423696de9cd7bf79e
laura De la rosa <laura.dlr.cardona@gmail.com>
2019-11-23 12:53:52 -0500

    implementacion del gps y muestra el pais actual en un textview

--- app/src/main/java/com/grupo7/moneychange/ui/MainActivity.kt
+++ app/src/main/java/com/grupo7/moneychange/ui/MainActivity.kt
@@ -1,13 +1,80 @@
 package com.grupo7.moneychange.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.grupo7.moneychange.R
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.BasePermissionListener
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val location = getLocation()
+            countryName?.text = getCountryFromLocation(location)
+        }
+
     }
+
+    private suspend fun getLocation(): Location? {
+        val success = requestPermission()
+        return if (success) findLastLocation() else null
+    }
+
+    private suspend fun requestPermission(): Boolean =
+        suspendCancellableCoroutine { continuation ->
+            Dexter
+                .withActivity(this@MainActivity)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(object : BasePermissionListener() {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                        continuation.resume(true)
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                        continuation.resume(false)
+                    }
+                }).check()
+        }
+
+    private fun getCountryFromLocation(location: Location?): String {
+        val geocoder = Geocoder(this)
+        val fromLocation = location?.let {
+            geocoder.getFromLocation(location.latitude, location.longitude, 1)
+        }
+        return fromLocation?.firstOrNull()?.countryName ?: "Colombias"
+
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private suspend fun findLastLocation(): Location? =
+        suspendCancellableCoroutine { continuation ->
+            fusedLocationClient.lastLocation.addOnCompleteListener {
+                continuation.resume(it.result)
+            }
+        }
+
+
 }
