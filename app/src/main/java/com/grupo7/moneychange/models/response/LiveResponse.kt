adc4f55f40885927a2709012a09993ad797d3086
Juan Pablo Vivas Reinoso <jvivas@merqueo.com>
2019-11-26 21:31:57 -0500

    [Feat] Refactor response LiveResponse

--- app/src/main/java/com/grupo7/moneychange/models/response/LiveResponse.kt
+++ app/src/main/java/com/grupo7/moneychange/models/response/LiveResponse.kt
@@ -1,25 +1,10 @@
 package com.grupo7.moneychange.models.response
 
-import com.squareup.moshi.Json
-
 data class LiveResponse(
     val privacy: String,
-    val quotes: Quotes,
+    val quotes: MutableMap<String, Double>,
     val source: String,
     val success: Boolean,
     val terms: String,
     val timestamp: Int
-)
-
-data class Quotes(
-    @Json(name = "USDAED")
-    val usdAed: Double,
-    @Json(name = "USDAFN")
-    val usdAfn: Double,
-    @Json(name = "USDALL")
-    val usdAll: Double,
-    @Json(name = "USDAMD")
-    val usdAmd: Double,
-    @Json(name = "USDANG")
-    val usdAng: Double
 )
