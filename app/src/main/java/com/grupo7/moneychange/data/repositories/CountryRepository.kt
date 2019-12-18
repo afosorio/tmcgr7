3a4745c23395acae7f89f45b0556fa63f34e5b3a
afosorio <af.osorio86@gmail.com>
2019-12-17 22:38:28 -0500

    Organización Paquetes (close #26)

--- app/src/main/java/com/grupo7/moneychange/data/repositories/CountryRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.grupo7.moneychange.data.repositories
-
-import com.grupo7.moneychange.utils.PermissionChecker
-
-interface CountryRepository {
-   suspend fun getCountryLocation(permissionChecker: PermissionChecker): String
-}
-
