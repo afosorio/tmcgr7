f8929befa514ae2d323d47991345035e44dd49aa
Laura De la Rosa <laura.dlr.cardona@gmail.com>
2020-03-02 21:21:11 -0500

    se agregan TODO en la calse de detailConversionViewModel

--- app/src/main/java/com/grupo7/moneychange/ui/detail/DetailConversionViewModel.kt
+++ app/src/main/java/com/grupo7/moneychange/ui/detail/DetailConversionViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.grupo7.data.repository.HistoryRepository
 import com.grupo7.domain.History
+import com.grupo7.moneychange.ui.entitiesUi.HistoryItem
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 
@@ -14,23 +15,27 @@ class DetailConversionViewModel(
 ) : ViewModel() {
 
 
-    private val _historyData = MutableLiveData<History>()
-    val historyData: LiveData<History> get() = _historyData
+    private val _historyData = MutableLiveData<HistoryItem>()
+    val historyData: LiveData<HistoryItem> get() = _historyData
 
 
     val dateTextView: MutableLiveData<String> = MutableLiveData()
 
     fun fetchHistoryById(id: Int) {
         viewModelScope.launch {
-            _historyData.value = historyRepository.findById(id)
+            //TODO evaluar si este viewmodel debe llamar un  repositorio??? o se le puede pasar la informacion de la moneda por argumentos? o si se debe llamar un caso de uso.
+            // TODO las siguientes lineas de codigo hay que refactorizarlas a como debe ser
+            val data: History = historyRepository.findById(id)
+            val history = HistoryItem(data.id, data.date, data.currencyFrom.toString(), data.currencyTo.toString(), data.valueFrom, data.valueTo)
+            _historyData.value = history
             setDateFormat()
         }
     }
 
-    private fun setDateFormat(){
-        if (historyData.value != null){
+    private fun setDateFormat() {
+        if (historyData.value != null) {
             val sdf = SimpleDateFormat("dd-MMMM-yyyy")
-            dateTextView.value  = sdf.format(historyData.value?.date)
+            dateTextView.value = sdf.format(historyData.value?.date)
         }
     }
 }
